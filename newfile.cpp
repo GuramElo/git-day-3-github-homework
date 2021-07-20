719c82b19cf46a457edeba8249926275b7768b8f
Guram Elizbarashvili <elizbarashvili.guram18@gmail.com>
2021-07-20 17:30:44 +0400

    not now

--- newfile.cpp
+++ newfile.cpp
@@ -1,9 +1,6 @@
-#include <bits/stdc++.h>
-using namespace std;
-int a,b,c,d,e;
-main(){
-    for(a=1;a<101;a++){
-        if(a%2==0)printf("%d is even\n",a);
-        else printf("%d is odd\n",a)
-    }
-}
+/* change 
+it
+into
+simple
+plain
+text*/
